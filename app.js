7290739196be33619aeabd26730be2578c1b8281
omkarpg0931 <omkar0931@gmail.com>
2018-05-07 14:57:17 +0530

    fixed path to angular app

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(expressValidator());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app')));
 
 app.use('/api/users', usersRouter);
 app.use('/api/recipes', recipesRouter);
