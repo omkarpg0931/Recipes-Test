d32464461694dfb9817b905c416105a9971fc9b4
Omkar PG <omkar0931@gmail.com>
2018-05-07 23:49:40 +0530

    fixed issue with login

--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/user');
 /* GET users listing. */
-router.get('/login', userController.login);
+router.post('/login', userController.login);
 
 router.post('/register', userController.register);
 
