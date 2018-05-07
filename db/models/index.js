e91942353d49feaa2add3234b002ae41330289a0
Omkar PG <omkar0931@gmail.com>
2018-05-07 17:42:14 +0530

    fixes issue with db connection

--- db/models/index.js
+++ db/models/index.js
@@ -8,14 +8,10 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../../config/config.json')[env];
 var db        = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable]);
-} else {
-  sequelize = new Sequelize(
+let sequelize = new Sequelize(
     config.database, config.username, config.password, config
   );
-}
+
 
 fs
   .readdirSync(__dirname)
